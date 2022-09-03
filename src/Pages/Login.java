b2e19bee49ebff3835a08f03eee50fa78deb33e2
Nazerke <nazomardc@gmail.com>
2022-09-02 23:51:10 -0500

    Nazerke update Login class

--- src/Pages/Login.java
+++ src/Pages/Login.java
@@ -4,5 +4,6 @@ public class Login {
     public static void main(String[] args) {
         System.out.println();
         //Nazerke created Login class
+        //Nazerke updated Login class
     }
 }
